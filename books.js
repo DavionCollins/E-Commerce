60600ee7ea4c11aa43500f7430609c703720bbee
DavionCollins <davioncollins3461@gmail.com>
2025-12-08 12:54:24 -0600

    Created a filter for the books

--- books.js
+++ books.js
@@ -1,26 +1,54 @@
-function renderBooks() {
+function renderBooks(filter) {
   const booksWrapper = document.querySelector('.books')
 
-
 const books = getBooks()
-console.log(books)
-  booksWrapper.innerHTML =
-  `<div class="book">
+
+if (filter === 'LOW_TO_HIGH') {
+  books.sort((a, b) => (a.originalPrice) - (b.originalPrice))
+}
+else if (filter === 'HIGH_TO_LOW') {
+  books.sort((a, b) => (b.originalPrice) - (a.originalPrice))
+}
+else if (filter === 'RATING') {
+  books.sort((a, b) => (b.rating) - (a.rating))
+}
+
+
+
+
+
+const booksHtml = books.map(book => {
+  return `<div class="book">
     <figure class="book__img--wrapper">
-        <img class="book__img" src="${books[0].url}" alt="">
+        <img class="book__img" src="${book.url}" alt="">
     </figure>
-    <div class="book__title">${books[0].title}</div>
+    <div class="book__title">${book.title}</div>
     <div class="book__ratings">
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star-half-alt"></i>
+        ${ratingHtml(book.rating)}
     </div>
     <div class="book__price">
-        <span class="book__price--normal">${books[0].originalPrice}</span>${books[0].salePrice}
+        <span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice}
     </div>
 </div>`
+}).join("")
+
+  booksWrapper.innerHTML = booksHtml
+}
+
+function filterBooks(event){
+renderBooks(event.target.value)
+}
+
+function ratingHtml(rating) {
+  let ratingHtml = "";
+  for (let i = 0; i < Math.floor(rating); i++) {
+  ratingHtml += '<i class="fa-solid fa-star"></i>'
+}
+
+if (!Number.isInteger(rating)){
+  ratingHtml += '<i class="fa-solid fa-star-half-alt"></i>'
+}
+return ratingHtml
 }
 
 setTimeout(() => {
@@ -74,7 +102,7 @@ function getBooks() {
       title: "Rich Dad Poor Dad",
       url: "E-Commerce Assets/assets/book-3.jpeg",
       originalPrice: 70,
-      salePrice: 12.5,
+      salePrice: 12.50,
       rating: 5,
     },
     {
